fa266435ddf4eac07e662686189c0c949ff10147
Stephen Leisy <stephenleisy@Stephens-MacBook-Pro.local>
2021-12-22 16:16:49 -0800

    dummy data in one treatment card 2

--- pages/index.tsx
+++ pages/index.tsx
@@ -66,6 +66,10 @@ export default function Home() {
         image={
           'https://images.theconversation.com/files/423138/original/file-20210924-19-1ryf4ed.jpg?ixlib=rb-1.1.0&rect=8%2C25%2C5742%2C3802&q=20&auto=format&w=320&fit=clip&dpr=2&usm=12&cs=strip'
         }
+        text={''}
+        moreText={''}
+        textThree={''}
+        textFour={''}
       />
     </>
   );
